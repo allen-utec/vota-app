24f7ccd2126842bb7fe62345b54d823c2f1fcacc
Allen Joseph <allen.velasco@utec.edu.pe>
2023-05-11 23:29:55 -0500

    feat: call vote api and show results

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,31 +1,44 @@
 export async function getNickname() {
-  return fetch("http://127.0.0.1:9090/nicknames")
-      .then((o) => o.json());
+  return fetch("http://127.0.0.1:9090/nicknames").then((o) => o.json());
 }
 
-export async function createUser(user: {nickname: string}) {
+export async function createUser(user: { nickname: string }) {
   return fetch("http://127.0.0.1:8080/users", {
-      method: "POST",
-      body: JSON.stringify(user),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json());
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
 }
 
-export async function createPoll(poll: {question: string, alternatives: {text:string}[], user_id: string}) {
+export async function createPoll(poll: {
+  question: string;
+  alternatives: { text: string }[];
+  user_id: string;
+}) {
   return fetch("http://127.0.0.1:8080/polls", {
-      method: "POST",
-      body: JSON.stringify(poll),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json());
+    method: "POST",
+    body: JSON.stringify(poll),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
 }
 
 export async function getPollByCode(code: string) {
-  return fetch(`http://127.0.0.1:8080/polls/${code}`)
-      .then((res) => res.json());
+  return fetch(`http://127.0.0.1:8080/polls/${code}`).then((res) => res.json());
+}
+
+export async function sendMyVote(pollId: number, alternativeId: number) {
+  let user = JSON.parse(localStorage.getItem("user"));
+  if (!user.id) Promise.reject();
+
+  return fetch(`http://127.0.0.1:8080/polls/${pollId}`, {
+    method: "POST",
+    body: JSON.stringify({ user_id: user.id, alternative_id: alternativeId }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
 }
--- src/pages/Vote.svelte
+++ src/pages/Vote.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import Button from "../components/Button.svelte";
   import Header from "../components/Header.svelte";
-  import { getPollByCode } from "../lib/api";
+  import { getPollByCode, sendMyVote } from "../lib/api";
 
   export let code: string;
 
-  let poll: any;
-  let optionIdSelected = null;
+  let poll: { id: number; question: string; alternatives: any[] };
+  let alternativeIdSelected = null;
+
+  let results: Record<number, number> = null;
+  let totalVotes: number = 0;
 
   if (code) {
     getPollByCode(code)
@@ -14,12 +17,21 @@
       .catch(console.log);
   }
 
-  const handleSelectOption = (option: { id: number }) => {
-    optionIdSelected = option.id;
+  const handleSelectOption = (alternative: { id: number }) => {
+    alternativeIdSelected = alternative.id;
   };
 
   const handleSend = () => {
-    // TODO: show a modal with a share button
+    sendMyVote(poll.id, alternativeIdSelected)
+      .then((o: { votes: number[] }) => {
+        totalVotes = o.votes.length;
+
+        results = o.votes.reduce((acc, val) => {
+          acc[val] = (acc[val] || 0) + 1;
+          return acc;
+        }, {} as Record<number, number>);
+      })
+      .catch(console.log);
   };
 
   const handleNewPoll = () => {
@@ -38,21 +50,38 @@
       Alternativas a votar:
     </p>
     <ol class="space-y-2 font-light mb-4 mx-auto">
-      {#each poll.alternatives as option}
+      {#each poll.alternatives as alternative}
         <li
-          class={`flex items-center gap-1 rounded-md p-4 cursor-pointer border ${
-            option.id === optionIdSelected
+          class={`relative flex items-center gap-1 rounded-md p-4 border ${
+            results ? "cursor-not-allowed" : "cursor-pointer"
+          } ${
+            !results && alternative.id === alternativeIdSelected
               ? "bg-green-50 border-green-500"
               : "border-gray-300"
           }`}
-          on:click={handleSelectOption.bind(null, option)}
+          on:click={!results && handleSelectOption.bind(null, alternative)}
         >
-          {option.text}
+          {#if results}
+            <span
+              class="absolute flex items-center p-4 left-0 top-0 h-full rounded-md border bg-blue-100 border-blue-500"
+              style={`width: ${(results[alternative.id] / totalVotes) * 100}%`}
+            />
+          {/if}
+          <span class="z-10">
+            {alternative.text}
+            {#if results}
+              <span class="ml-2 font-bold">
+                [ {((results[alternative.id] / totalVotes) * 100).toFixed(2)}% ]
+              </span>
+            {/if}
+          </span>
         </li>
       {/each}
     </ol>
-    <Button on:click={handleSend} disabled={!poll.alternatives.length}>
-      Enviar Mi Voto!
-    </Button>
+    {#if !results}
+      <Button on:click={handleSend} disabled={!poll.alternatives.length}>
+        Enviar Mi Voto!
+      </Button>
+    {/if}
   {/if}
 </div>
